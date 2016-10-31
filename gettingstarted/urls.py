266d77498325c27a58177efc0c6d462925fa9e0e
Jose <jootse.84@gmail.com>
2016-10-31 11:52:59 -0400

    fix urls

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -10,7 +10,7 @@ import hello.views
 # url(r'^blog/', include('blog.urls')),
 
 urlpatterns = [
-    url(r'^image2', hello.views.index, name='image1'),
-    url(r'^image1', hello.views.db, name='image2'),
+    url(r'^image2', hello.views.image2, name='image1'),
+    url(r'^image1', hello.views.image1, name='image2'),
     url(r'^admin/', include(admin.site.urls)),
 ]
